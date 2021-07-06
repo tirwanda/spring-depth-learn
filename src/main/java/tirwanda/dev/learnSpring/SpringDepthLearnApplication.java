7081dd54d3e8ab60f1ad959cf6c52af4c2e64231
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-07-06 22:50:04 +0700

    First Commit

---
+++ src/main/java/tirwanda/dev/learnSpring/SpringDepthLearnApplication.java
@@ -0,0 +1,13 @@
+package tirwanda.dev.learnSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDepthLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDepthLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tirwanda/dev/learnSpring/SpringDepthLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package tirwanda.dev.learnSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDepthLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
